6358cd3cd3f3b12a23ee46d202b2bd396f270945
Malcome Fein <mfein90@gmail.com>
2018-08-08 12:58:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,16 +4,16 @@ class Dog
     @name
   end
   
-  def name = (name)
-    @name = name
-  end 
+  # def name = (name)
+  #   @name = name
+  # end 
   
-  def breed
-    @breed
-  end
+  # def breed
+  #   @breed
+  # end
 
-  def breed = (breed)
-    @breed = breed
-  end
+  # def breed = (breed)
+  #   @breed = breed
+  # end
  
 end
